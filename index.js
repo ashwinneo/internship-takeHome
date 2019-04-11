525d02e622a02620fe0fcbc7c9489d9fa1a46c9c
Ashwin <ashwinneo@gmail.com>
2019-04-11 03:35:15 -0700

    Offline Mode Change & Image Upload Bug Fixes

--- index.js
+++ index.js
@@ -7,35 +7,46 @@ var cors = require('cors')
 app.use(cors())
 
 const sqlite3 = require('sqlite3').verbose();
-let db = new sqlite3.Database('./db/user.sqlite', sqlite3.OPEN_READWRITE, (err) => {
-    if (err) {
-      console.error(err.message);
-    }
-    console.log('Connected to the MY database.');
+let db = new sqlite3.Database('./db/DB.sqlite', sqlite3.OPEN_READWRITE, (err) => {
+  if (err) {
+    console.error(err.message);
+  }
+  console.log('Connected to the MY database.');
 });
 
 app.get('/getUsers', (request, response) => {
-    db.all("SELECT * FROM USER", function(err,rows,fields) {
-        response.setHeader('Content-Type','application/json')
-        console.log(rows);
-        response.status(200).send(JSON.stringify(rows));
-    });
-  })
+  db.all("SELECT * FROM USERS", function (err, rows, fields) {
+    response.setHeader('Content-Type', 'application/json')
+    response.status(200).send(JSON.stringify(rows));
+  });
+})
 
 app.post('/createImage', (req, res) => {
-    const id = Math.floor(Math.random() * 100);
-    const file_name = req.body.file_name;
-    const annotation_name = req.body.annotation_name;
-    var inputData = [id,file_name,annotation_name];
-    db.run(`INSERT INTO USER(id,file_name,annotation_name) VALUES(?,?,?)`, inputData, function(err,rows) {
+  for (var i = 0; i < req.body.length; i++) {
+    if (!req.body[i].fileName) {
+      return res.status(200).send({
+        errorId: 1100,
+        message: "File name cannot be empty"
+      })
+    } else if (!req.body[i].annotation_text) {
+      return res.status(200).send({
+        errorId: 1100,
+        message: "Annotation name cannot be empty"
+      })
+    } else {
+      const file_name = req.body[i].fileName;
+      const annotation_name = req.body[i].annotation_text;
+      var inputData = [file_name, annotation_name];
+      db.run(`INSERT INTO USERS(file_name,annotation_name) VALUES(?,?)`, inputData, function (err, rows) {
         if (err) {
           return console.log(err.message);
         }
-        // get the last insert id
       });
-    return res.status(200).send({
-        succes: "Success"
-    })
+      res.status(200).send({
+        success: "Image uploaded succesfully"
+      })
+    }
+  }
 });
 
 const PORT = 8081;
